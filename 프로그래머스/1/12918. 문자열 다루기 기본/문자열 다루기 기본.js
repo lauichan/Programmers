5b040e773e2af52fef2e92871dc796ce897e0588
nauichan <85791020+lauichan@users.noreply.github.com>
2024-01-08 12:01:43 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,7 +1,4 @@
 function solution(s) {
-    if (s.length !== 4 && s.length !== 6) return false;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] % 1 !== 0) return false;
-    }
-    return true;
+    var answer = true;
+    return answer;
 }
