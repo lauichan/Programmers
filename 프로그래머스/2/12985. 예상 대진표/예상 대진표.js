d907c21949a50ad56310f0db2deb952b149a8240
nauichan <85791020+lauichan@users.noreply.github.com>
2024-09-06 17:50:52 +0900

    [level 2] Title: 예상 대진표, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -2,7 +2,7 @@ function solution(n,a,b) {
     let aNum = a;
     let bNum = b;
     let round = 1;
-    while (true) {
+    while (round < n) {
         aNum = Math.ceil(aNum / 2);
         bNum = Math.ceil(bNum / 2);
         if (aNum === bNum) break;
