3fc6ab396d8754e7cfabace4dad2fe21a1784b35
nauichan <85791020+lauichan@users.noreply.github.com>
2024-09-09 21:43:13 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let arr = [0, 1, 2];
     for (let i = 3; i <= n; i++) {
-        arr.push((arr.at(-1) + arr.at(-2)) % 1234567);
+        arr.push((arr[i-1] + arr[i-2]) % 1234567)
     }
-    return arr[n];
+    return arr[n]
 }
